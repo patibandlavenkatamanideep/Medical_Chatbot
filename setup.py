527a437c1c001e49bf211fddb9e0c681cc8e7675
Venkata Manideep <venkatamanideep20@gmail.com>
2026-02-21 10:02:15 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical-chatbot',
+    version='0.0.1',
+    author="Venkata Manideep Patibandla",
+    author_email="venkatamanideep20@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
